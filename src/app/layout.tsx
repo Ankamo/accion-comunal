2b174ce008f550df41c7fcce77b6eb9a8551d59d
Ankamo <102838840+Ankamo@users.noreply.github.com>
2024-10-19 01:11:41 -0500

    Update V. 1.0.0.8

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 import '../styles/globals.css';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 import { ReactNode } from 'react';
 
 export const metadata = {
@@ -12,9 +10,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="es">
       <body className="bg-[#0a0403] text-white">
-        <Header />
         <main className="min-h-screen p-4">{children}</main>
-        <Footer />
       </body>
     </html>
   );

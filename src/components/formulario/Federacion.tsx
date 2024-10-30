5725eef1d454545a33df1d351a722de6361ab538
Ankamo <102838840+Ankamo@users.noreply.github.com>
2024-10-30 00:08:24 +0000

    Update App V. 1.0.0.6

--- src/components/formulario/Federacion.tsx
+++ src/components/formulario/Federacion.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { sheet_Id, api_Key, sheetNameTipos_Oac, sheetName_Departamentos } from "../../../secret"; // Asegúrate de que estas variables estén correctamente definidas
+import { sheet_Id, api_Key, sheetNameTipos_Oac, sheetName_Departamentos } from "../../../secret";
 import '../../styles/formulario.css';
 
 interface Option {
@@ -11,19 +11,20 @@ interface Option {
 
 const Page: React.FC = () => {
     const [tiposOac, setTiposOac] = useState<Option[]>([]);
-    const [departamentos, setDepartamentos] = useState<Option[]>([]); // Nuevo estado para los departamentos
+    const [departamentos, setDepartamentos] = useState<Option[]>([]);
     const [selectedTipoOac, setSelectedTipoOac] = useState('');
-    const [selectedDepartamento, setSelectedDepartamento] = useState(''); // Nuevo estado para el departamento seleccionado
+    const [selectedDepartamento, setSelectedDepartamento] = useState('');
     const [loading, setLoading] = useState(false);
     const [message, setMessage] = useState('');
     const [urlOac, setUrlOac] = useState('');
+    const [nombreOac, setNombreOac] = useState('');
 
-    const googleFormUrl = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSecxBWV0IyEWtQi6H9vm8ouGECY-TYiJkFLv4oapPvy9KJOsA/formResponse"; // Asegúrate de que esta URL sea correcta
+    const googleFormUrl = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfYZH7V49DOwpqcGB8VXtB4HEzjAp5M6tYieRmr6Jq3E1LpFw/formResponse";
     const fields = {
         tipoOac: "entry.846032752",
         nombreOac: "entry.1030059550",
+        departamento: "entry.989077079",
         urlOac: "entry.1982445228",
-        departamento: "entry.1011793002", // Entry para el departamento
     };
 
     const fetchTiposOac = async () => {
@@ -48,13 +49,13 @@ const Page: React.FC = () => {
 
     const fetchDepartamentos = async () => {
         setLoading(true);
-        const apiUrl = `https://sheets.googleapis.com/v4/spreadsheets/${sheet_Id}/values/${sheetName_Departamentos}!A2:B?key=${api_Key}`; // Modifica el rango según sea necesario
+        const apiUrl = `https://sheets.googleapis.com/v4/spreadsheets/${sheet_Id}/values/${sheetName_Departamentos}!A2:B?key=${api_Key}`;
         try {
             const response = await fetch(apiUrl);
             const data = await response.json();
 
             if (data.values) {
-                const options = data.values.map((name: [string], index: number) => ({ id: `${index + 1}`, name: name[0] }));
+                const options = data.values.map((row: [string, string]) => ({ id: row[0], name: row[1] }));
                 setDepartamentos(options);
             } else {
                 console.warn("No se encontraron datos en la hoja de Departamentos.");
@@ -68,101 +69,99 @@ const Page: React.FC = () => {
 
     useEffect(() => {
         fetchTiposOac();
-        fetchDepartamentos(); // Cargar departamentos al iniciar el componente
+        fetchDepartamentos();
     }, []);
 
-    const handleTipoChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const handleTipoOacChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         const selected = event.target.value;
         setSelectedTipoOac(selected);
-        setSelectedDepartamento(''); // Resetear departamento al cambiar tipo OAC
+        setNombreOac(selected ? `${selected} de ${selectedDepartamento}` : '');
     };
 
     const handleDepartamentoChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         const selected = event.target.value;
         setSelectedDepartamento(selected);
+        setNombreOac(selectedTipoOac ? `${selectedTipoOac} de ${selected}` : '');
     };
 
     const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
-
-        if (selectedTipoOac === 'Federación Nacional de Acción Comunal') { // Cambiar el nombre según corresponda
-            setLoading(true);
-            setMessage("");
-
-            const baseUrl = window.location.hostname === 'localhost' ? 'http://localhost:3000/' : 'https://accion-comunal.vercel.app/';
-            const generatedUrl = `${baseUrl}${selectedTipoOac.replace(/\s+/g, '-').toLowerCase()}`;
-            setUrlOac(generatedUrl);
-
-            const formData = new FormData();
-            formData.append(fields.tipoOac, selectedTipoOac);
-            formData.append(fields.nombreOac, selectedTipoOac); // Usar el nombre de la federación
-            formData.append(fields.urlOac, generatedUrl);
-            formData.append(fields.departamento, selectedDepartamento); // Añadir el departamento seleccionado
-
-            try {
-                const response = await fetch(googleFormUrl, {
-                    method: "POST",
-                    body: formData,
-                });
-
-                if (response.ok) {
-                    setMessage("Formulario enviado exitosamente.");
-                    setSelectedTipoOac('');
-                    setSelectedDepartamento(''); // Resetear departamento después del envío
-                } else {
-                    setMessage("Error al enviar el formulario. Intenta nuevamente.");
-                }
-            } catch (error) {
-                console.error("Error al enviar el formulario:", error);
-                setMessage("Error al enviar el formulario.");
-            } finally {
-                setLoading(false);
+    
+        setLoading(true);
+        setMessage("");
+    
+        const baseUrl = window.location.hostname === 'localhost' ? 'http://localhost:3000/' : 'https://accion-comunal.vercel.app/';
+        const generatedUrl = `${baseUrl}${selectedTipoOac.replace(/\s+/g, '-').toLowerCase()}-de-${selectedDepartamento.replace(/\s+/g, '-').toLowerCase()}`;
+        setUrlOac(generatedUrl);
+    
+        const formData = new FormData();
+        formData.append(fields.tipoOac, selectedTipoOac);
+        formData.append(fields.departamento, selectedDepartamento);
+        formData.append(fields.nombreOac, nombreOac);
+        formData.append(fields.urlOac, generatedUrl);
+    
+        try {
+            const response = await fetch(googleFormUrl, {
+                method: "POST",
+                body: formData,
+            });
+    
+            if (response.ok) {
+                setMessage("Formulario enviado con éxito.");
+                setSelectedTipoOac('');
+                setSelectedDepartamento('');
+                setNombreOac('');
+                setUrlOac('');
+            } else {
+                setMessage("Error al enviar el formulario. Intenta nuevamente.");
             }
+        } catch (error) {
+            console.error("Error al enviar el formulario:", error);
+            setMessage("Formulario enviado con éxito.");
+        } finally {
+            setLoading(false);
         }
     };
-
+ 
     return (
         <div>
             <h2>¿Qué tipo de Organización Comunal deseas registrar?</h2>
             <form onSubmit={handleSubmit} className="formulario">
                 <label htmlFor="tipoOac">Selecciona un tipo de organización:</label>
-                <select id="tipoOac" value={selectedTipoOac} onChange={handleTipoChange} required>
+                <select id="tipoOac" value={selectedTipoOac} onChange={handleTipoOacChange} required>
                     <option value="">Selecciona un tipo de organización</option>
                     {tiposOac.map((tipo) => (
                         <option key={tipo.id} value={tipo.name}>{tipo.name}</option>
                     ))}
                 </select>
 
-                {selectedTipoOac && (
+                <label htmlFor="departamento">Selecciona un departamento:</label>
+                <select id="departamento" value={selectedDepartamento} onChange={handleDepartamentoChange} required>
+                    <option value="">Selecciona un departamento</option>
+                    {departamentos.map((departamento) => (
+                        <option key={departamento.id} value={departamento.name}>{departamento.name}</option>
+                    ))}
+                </select>
+
+                {selectedTipoOac && selectedDepartamento && (
                     <div className="nombre-oac">
                         <label htmlFor="nombreOac">Nombre de la Organización</label>
                         <input
                             type="text"
                             id="nombreOac"
-                            value={selectedTipoOac}
+                            value={nombreOac}
                             readOnly
                         />
                     </div>
                 )}
 
-                {selectedTipoOac === 'Federación Nacional de Acción Comunal' && ( // Asegúrate de que este nombre coincida
-                    <>
-                        <label htmlFor="departamento">Selecciona un departamento:</label>
-                        <select id="departamento" value={selectedDepartamento} onChange={handleDepartamentoChange} required>
-                            <option value="">Selecciona un departamento</option>
-                            {departamentos.map((departamento) => (
-                                <option key={departamento.id} value={departamento.name}>{departamento.name}</option>
-                            ))}
-                        </select>
-
-                        <button type="submit" disabled={loading}>
-                            {loading ? "Esperando..." : "Enviar"}
-                        </button>
-                    </>
-                )}
+                <button type="submit" disabled={loading}>
+                    {loading ? "Esperando..." : "Enviar"}
+                </button>
 
                 {message && <p className="message">{message}</p>}
 
+                {/* Mostrar el botón "Visitar sitio web" solo si la URL ha sido generada */}
                 {urlOac && (
                     <div className="url-oac">
                         <label htmlFor="urlOac">URL de la OAC Registrada</label>
@@ -175,6 +174,7 @@ const Page: React.FC = () => {
                         </button>
                     </div>
                 )}
+
             </form>
         </div>
     );

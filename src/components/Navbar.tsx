f804505d169a2beada44c6dbe0b2474f205902d1
Ankamo <102838840+Ankamo@users.noreply.github.com>
2024-11-01 22:49:05 +0000

    Update App V. 1.0.1.5

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,22 +10,23 @@ import '../styles/navbar.css'; // Importa el archivo CSS
 const Navbar = () => {
     const pathname = usePathname(); // Captura la ruta actual
 
-    // Captura la OAC actual de la URL
-    const segments = pathname.split('/').filter(Boolean); // Filtra cualquier segmento vacío
-    const oacPath = segments.length > 0 ? segments[0] : ''; // Asegúrate de que haya segmentos
+    // Verifica si la ruta actual es la página principal
+    if (pathname === '/') return null;
 
     // Define las rutas del navbar según la OAC actual
+    const segments = pathname.split('/');
+    const oacPath = segments[1]; // Esto asume que la OAC es el primer segmento de la URL
     const navLinks = [
-        { label: 'Inicio', href: `/${oacPath}` }, // Redirige a la página principal de la OAC
+        { label: 'Inicio', href: `/${oacPath}` },
         { label: 'Cartelera Comunal', href: `/${oacPath}/cartelera` },
         { label: 'Eventos y Actividades', href: `/${oacPath}/eventos-y-actividades` },
         { label: 'Reuniones', href: `/${oacPath}/reuniones` },
         { label: 'Miembros Afiliados', href: `/${oacPath}/miembros-afiliados` },
         { label: 'Proyectos Comunales', href: `/${oacPath}/proyectos-comunales` },
         { label: 'Servicios Comunales', href: `/${oacPath}/servicios-comunales` },
-        { label: '¿Quiénes Somos?', href: `/${oacPath}/quienes-somos` },
+        { label: 'Quiénes Somos', href: `/${oacPath}/quienes-somos` },
         { label: 'Ingresar', href: `/${oacPath}/ingresar` },
-        { label: 'Asistente Virtual', href: `/${oacPath}/asistente-virtual` },
+        { label: 'Asistente Virtual', href: `/${oacPath}/asistente-virtual` }
     ];
 
     return (

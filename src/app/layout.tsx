ec8d229a6e2f57e69d4b0435caae66b14108621b
Ankamo <102838840+Ankamo@users.noreply.github.com>
2024-11-15 21:55:15 -0500

    Create App V. 1.0.0.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,12 @@
 
 import React from 'react';
 import '../styles/globals.css'; // Importa los estilos globales
-import HeaderNav from '../components/HeaderNav'; // Importa el HeaderNavbar
 import Footer from '../components/Footer';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <html lang="es">
             <body>
-                <HeaderNav /> {/* Cambia a Header */}
                 <main>{children}</main>
                 <Footer />
             </body>

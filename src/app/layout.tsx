a5ce12aad90faaeb8a44ddfe96cc4fbdabb3f8a9
Ankamo <102838840+Ankamo@users.noreply.github.com>
2024-11-15 23:57:40 -0500

    Update New Version 1.0.0.4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,14 @@ import React from 'react';
 import '../styles/globals.css'; // Importa los estilos globales
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import Nav from '../components/Nav';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <html lang="es">
             <body>
                 <Header />
+                <Nav />
                 <main>{children}</main>
                 <Footer />
             </body>
